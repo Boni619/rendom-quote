40b52aa94bc761799ff6c07c5bbb73e0fd69ee17
Boni <boni619@gmail.com>
2019-08-15 11:41:55 +0530

    Updates

---
+++ precache-manifest.6f7fea5f4986e3be592abb5a90107c92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd18816ddca16d433af724da0bbd2b98",
+    "url": "/rendom-quote/index.html"
+  },
+  {
+    "revision": "7aa127d6bc82d92e4ada",
+    "url": "/rendom-quote/static/css/main.48765cd5.chunk.css"
+  },
+  {
+    "revision": "eea785aecfa915437cb5",
+    "url": "/rendom-quote/static/js/2.0bbcfaee.chunk.js"
+  },
+  {
+    "revision": "7aa127d6bc82d92e4ada",
+    "url": "/rendom-quote/static/js/main.59803470.chunk.js"
+  },
+  {
+    "revision": "19e1c8d25aae29b2a87f",
+    "url": "/rendom-quote/static/js/runtime~main.b581be12.js"
+  }
+]);
