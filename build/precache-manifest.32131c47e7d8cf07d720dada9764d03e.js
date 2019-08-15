c4ee3197a176b9f8dfc395d39e84f871761fcaf4
Boni <boni619@gmail.com>
2019-08-15 11:37:56 +0530

    rendom quotes code added

---
+++ build/precache-manifest.32131c47e7d8cf07d720dada9764d03e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea669cecabe3461525286fd5c11d75b",
+    "url": "/rendom-quote/index.html"
+  },
+  {
+    "revision": "8f8cd094b644a533ba3c",
+    "url": "/rendom-quote/static/css/main.48765cd5.chunk.css"
+  },
+  {
+    "revision": "eea785aecfa915437cb5",
+    "url": "/rendom-quote/static/js/2.0bbcfaee.chunk.js"
+  },
+  {
+    "revision": "8f8cd094b644a533ba3c",
+    "url": "/rendom-quote/static/js/main.908aac98.chunk.js"
+  },
+  {
+    "revision": "19e1c8d25aae29b2a87f",
+    "url": "/rendom-quote/static/js/runtime~main.b581be12.js"
+  }
+]);
